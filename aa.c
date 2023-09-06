0ba7eac90846a8e7cc2e426725a31319b8422718
Aka <87071843+ak0237@users.noreply.github.com>
2023-09-06 14:27:47 -0300

    morte e genes
    Adicionado a interação de morte. Genes em todos os indivíduos. E probabilidade de morte dependente do tipo de gene (Dominante ou recessivo)

--- aa.c
+++ aa.c
@@ -7,7 +7,7 @@
 #include <gsl/gsl_statistics.h>
 
 
-#define NI 10000 // numero de individuos
+#define NI 5000 // numero de individuos
 #define TR 1.0 // tamanho da rede
 #define R 0.01 // raio de interacao
 #define Rm 0.4 // raio de movimento
@@ -16,8 +16,8 @@
 
 #define LS 0
 
-#define sigma 0.00447213595499957939
-#define eta 1.0
+#define pA_die 0.02
+#define pa_die 0.01
 
 #define r (double)NG/NF
 #define q pow(NG, 1.0/(NF-1))
@@ -27,24 +27,36 @@ struct INDV{
 	double x; // posicao x do individuo
 	double y; // posicao y do individuo
 	int sexo;
+	double p_die;
+	int gene;
 	//int teste;
 };
 
 void ic(const gsl_rng *w, struct INDV *in){
 	int c;
+	double g;
 	
 	// Criando  individuos em posicoes aleatorias
-	for(c = 0; c < 10; c++){
+	for(c = 0; c < 200; c++){
 		in[c].ezst = 1;
 		in[c].x=  gsl_rng_uniform(w);
 		in[c].y=  gsl_rng_uniform(w);
+		in[c].p_die = pA_die;
 		if(gsl_rng_uniform(w) < 0.5){
 			in[c].sexo = 1;
 		}else{
 			in[c].sexo = 2;
 		}
-		//in[c].teste = c;
-
+		g = gsl_rng_uniform(w);
+		if(g < 0.5)
+			in[c].gene = 10;
+		else if(g < 0.75)
+			in[c].gene =11;
+		else{
+			in[c].gene = 0;
+			in[c].p_die = pa_die;
+		}
+		//printf("%e\n", in[c].p_die);
 	}
 }
 
@@ -62,13 +74,58 @@ void op(int t, struct INDV *in){
 
 	for(c = 0; c < NI; c++){
 		if(in[c].ezst != 0)
-			fprintf(file, "%e %e %d\n", in[c].x, in[c].y, in[c].sexo);
+			fprintf(file, "%e %e %d\n", in[c].x, in[c].y, in[c].gene);
 	}
 	fclose(file);
 }
 
+void genes(struct INDV *in, int combinacao, const gsl_rng *w, int fio){
+	double p_gene = gsl_rng_uniform(w);
+	switch(combinacao){
+	case 22:
+		in[fio].gene = 11;
+		break;
+	case 21:
+		if(p_gene < 0.5){
+			in[fio].gene = 11;
+		}else{
+			in[fio].gene = 10;
+		}
+		break;
+	case 20:
+		if(p_gene < 0.5){
+			in[fio].gene = 10;
+		}else{
+			if(p_gene < 0.75){
+				in[fio].gene = 11;
+			}else{
+				in[fio].gene =0;
+			}
+		}
+		break;
+	case 11:
+		in[fio].gene = 10;
+		break;
+	case 10:
+		if(p_gene < 0.5){
+			in[fio].gene = 10;
+		}else{
+			in[fio].gene = 0;
+		}
+		break;
+	case 0:
+		in[fio].gene = 0;
+		break;
+	}
+	if(in[fio].gene == 0)
+		in[fio].p_die = pa_die;
+	else
+		in[fio].p_die = pA_die;
+}
+
 int main(int argc, char **argv){
 	int n, j, t, l, f;
+	int AA, Aa, aa;
 	double dy, dx, dxm, dym, angulo, md;
 	struct INDV *in;
 
@@ -91,114 +148,129 @@ int main(int argc, char **argv){
 
 	l = 0;
 
+	char name[100];
+	FILE *file;
+	sprintf(name, "datg/p.dat");
+	file = fopen(name, "w");
+
+	
+
 	for(t = 1; t <= NG; t++){
 		
 		for(n = 0; n < NI; n++){
-			md = 20.0;
-			for(j = 0; j <NI; j++){
-				if(in[j].ezst == 1 && j != n){
-					dx = in[j].x - in[n].x;
-					if(fabs(dx) > TR * 0.5 && dx > 0)
-						dx -= TR;
-					else if(fabs(dx) > 0.5 * TR && dx < 0)
-						dx += TR;
-					//printf("dx = %e", dx);
-					dy = in[j].y - in[n].y;
-					if(fabs(dy) > TR * 0.5 && dy > 0)
-						dy -= TR;
-					else if(fabs(dy) > TR * 0.5 && dy < 0)
-						dy += TR;
-					//printf(" dy = %e", dy);
-					if(sqrt(dx*dx+dy*dy) < md && in[n].sexo != in[j].sexo){
-						md = sqrt(dx*dx+dy*dy);
-						//printf(" md = %e", md);
-						dxm = dx;
-						dym = dy;
-					}
-					//printf(" angulo = %e", angulo);
-					//printf("\n====================\n");
-				}
-			}
-			if(dxm > 0){
-				angulo = atan2(dym, dxm);
-			}else if(dxm < 0){
-				if(dym != 0){
-					angulo = atan2(dym, dxm);
-				}else{
-					angulo = M_PI;
-				}
-			}else{
-				if(dym > 0){
-					angulo = M_PI/2;
-				}else if(dym < 0){
-					angulo = -M_PI/2;
-				}else{
-					angulo = 0;
-				}
-			}
-			if(dym == 0 && dxm ==0){
-				in[n].x += (gsl_rng_uniform(w) * 0.01) + (gsl_rng_uniform(w) * (-0.01));
-				if(in[n].x>TR){
-					in[n].x -= TR;
-				}
-				if(in[n].x<0){
-					in[n].x += TR;
-				}
-				in[n].y += (gsl_rng_uniform(w) * 0.01) + (gsl_rng_uniform(w) * (-0.01));
-				if(in[n].y>TR){
-					in[n].y -= TR;
-				}
-				if(in[n].y<0){
-					in[n].y += TR;
-				}		
-			}else{
-				in[n].x += cos(angulo)*(gsl_rng_uniform(w) * 0.01);
-				if(in[n].x>TR){
-					in[n].x -= TR;
-				}
-				if(in[n].x<0){
-					in[n].x += TR;
-				}
-				in[n].y += sin(angulo)*(gsl_rng_uniform(w) * 0.01);
-				if(in[n].y>TR){
-					in[n].y -= TR;
-				}
-				if(in[n].y<0){
-					in[n].y += TR;
-				}
-			}
-			
-			for(j = 0; j < NI; j++){
-				if(in[j].ezst == 1 && in[j].sexo != in[n].sexo){
-					dx = in[j].x - in[n].x;
-					if(dx>TR){dx-=TR;}
-					if(dx<0){dx+=TR;}
-					dy = in[j].y - in[n].y;
-					if(dy>TR){dy-=TR;}
-					if(dy<0){dy+=TR;}
-					if(sqrt(dx*dx+dy*dy) < R){
-						for(f = 0; f < NI; f++){
-							if(in[f].ezst == 0){
-								in[f].ezst = 1;
-								if(in[n].x > in[j].x){
-									in[f].x = in[j].x + (0.5*dx);
-								}else{
-									in[f].x = in[n].x + (0.5*dx);
-								}
-								if(in[n].y > in[j].y){
-									in[f].y = in[j].y + (0.5*dy);
-								}else{
-									in[f].y = in[n].y + (0.5*dy);
-								}
-								if(gsl_rng_uniform(w) < 0.5)
-									in[f].sexo = 1;
-								else
-									in[f].sexo = 2;
-								f += NI;
-							}
+			if(in[n].ezst == 1){
+				AA = Aa = aa = 0;
+				md = 20.0;
+				for(j = 0; j <NI; j++){
+					if(in[j].ezst == 1 && j != n){
+						dx = in[j].x - in[n].x;
+						if(fabs(dx) > TR * 0.5 && dx > 0)
+							dx -= TR;
+						else if(fabs(dx) > 0.5 * TR && dx < 0)
+							dx += TR;
+						//printf("dx = %e", dx);
+						dy = in[j].y - in[n].y;
+						if(fabs(dy) > TR * 0.5 && dy > 0)
+							dy -= TR;
+						else if(fabs(dy) > TR * 0.5 && dy < 0)
+							dy += TR;
+						//printf(" dy = %e", dy);
+						if(sqrt(dx*dx+dy*dy) < md && in[n].sexo != in[j].sexo){
+							md = sqrt(dx*dx+dy*dy);
+							//printf(" md = %e", md);
+							dxm = dx;
+							dym = dy;
 						}
-						j += NI;
+						//printf(" angulo = %e", angulo);
+						//printf("\n====================\n");
 					}
+				}
+				if(dxm > 0){
+					angulo = atan2(dym, dxm);
+				}else if(dxm < 0){
+					if(dym != 0){
+						angulo = atan2(dym, dxm);
+					}else{
+						angulo = M_PI;
+					}
+				}else{
+					if(dym > 0){
+						angulo = M_PI/2;
+					}else if(dym < 0){
+						angulo = -M_PI/2;
+					}else{
+						angulo = 0;
+					}
+				}
+				if(dym == 0 && dxm ==0){
+					in[n].x += (gsl_rng_uniform(w) * 0.01) + (gsl_rng_uniform(w) * (-0.01));
+					if(in[n].x>TR){
+						in[n].x -= TR;
+					}
+					if(in[n].x<0){
+						in[n].x += TR;
+					}
+					in[n].y += (gsl_rng_uniform(w) * 0.01) + (gsl_rng_uniform(w) * (-0.01));
+					if(in[n].y>TR){
+						in[n].y -= TR;
+					}
+					if(in[n].y<0){
+						in[n].y += TR;
+					}		
+				}else{
+					in[n].x += cos(angulo)*(gsl_rng_uniform(w) * 0.01);
+					if(in[n].x>TR){
+						in[n].x -= TR;
+					}
+					if(in[n].x<0){
+						in[n].x += TR;
+					}
+					in[n].y += sin(angulo)*(gsl_rng_uniform(w) * 0.01);
+					if(in[n].y>TR){
+						in[n].y -= TR;
+					}
+					if(in[n].y<0){
+						in[n].y += TR;
+					}
+				}
+				
+				for(j = 0; j < NI; j++){
+					if(in[j].ezst == 1 && in[j].sexo != in[n].sexo){
+						dx = in[j].x - in[n].x;
+						if(dx>TR){dx-=TR;}
+						if(dx<0){dx+=TR;}
+						dy = in[j].y - in[n].y;
+						if(dy>TR){dy-=TR;}
+						if(dy<0){dy+=TR;}
+						if(sqrt(dx*dx+dy*dy) < R){
+							for(f = 0; f < NI; f++){
+								if(in[f].ezst == 0){
+									in[f].ezst = 1;
+									if(in[n].x > in[j].x){
+										in[f].x = in[j].x + (0.5*dx);
+									}else{
+										in[f].x = in[n].x + (0.5*dx);
+									}
+									if(in[n].y > in[j].y){
+										in[f].y = in[j].y + (0.5*dy);
+									}else{
+										in[f].y = in[n].y + (0.5*dy);
+									}
+									if(gsl_rng_uniform(w) < 0.5)
+										in[f].sexo = 1;
+									else
+										in[f].sexo = 2;
+									genes(in, in[n].gene + in[j].gene, w,f);
+									f += NI;
+								}
+							}
+							j += NI;
+						}
+					}
+				}
+				//printf("%e\n", in[n].p_die);
+				if(gsl_rng_uniform(w) < in[n].p_die){
+					in[n].ezst = 0; 
 				}
 			}
 		}
@@ -210,6 +282,20 @@ int main(int argc, char **argv){
 				a0+= r;
 			#endif
 				op(++l, in);
+				for(n = 0; n < NI; n++){
+					if(in[n].ezst !=0){
+						if(in[n].gene == 0)
+							aa++;
+						else if(in[n].gene == 10)
+							Aa++;
+						else
+							AA++;
+					}
+				}
+				fprintf(file, "%d %d %d %d\n", t, AA, Aa, aa);
+				
+				
 		}
 	}
+	fclose(file);
 }
